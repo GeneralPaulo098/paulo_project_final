943b22c8e236162f982b3d87752150d82ce375e9
Your Name <you@example.com>
2019-11-27 14:38:15 -0300

    terceira versao

--- final/views.py
+++ final/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
-from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 
 def home(request):
+    
+    soma = request.session.get('soma',0)
+    request.session['soma'] = soma + 1
+    conta = request.session['soma']
+    print(conta)
+    if conta == 1:
+        print(55)
+        messages.add_message(request, messages.INFO, 'bem vindo')
     return render(request,'home.html')
-    messages.add_message(request, messages.INFO, 'bem vindo')
+
+    
--- prova_final/settings.py
+++ prova_final/settings.py
@@ -120,3 +120,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/final/'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
