1e42f4db8d9c657ebb86763f98456ab128c6bae6
generalpaulo <generalpaulo098@gmail.com>
2019-11-26 21:52:23 -0300

    segunda versao

--- final/views.py
+++ final/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 
 def home(request):
     return render(request,'home.html')
+    messages.add_message(request, messages.INFO, 'bem vindo')
